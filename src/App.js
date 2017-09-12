16d04a8ff2473e311c834407acd07516872ac932
Alejandro Rubio <alejandrorubio@MacBook-Pro-2.local>
2017-09-12 11:24:18 -0700

    First edits to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import Shelf from './Shelf'
 import SearchBooks from './Search'
 //import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -18,7 +19,8 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Shelf />
+        <Route exact path="/" render={() => <Shelf />} />
+        <Route exact path="/search" render={() => <SearchBooks />} />
 
         {this.state.showSearchPage
           ? <SearchBooks />
